703185893ca88a5f3b95082b9aba7efb43a88f9b
Shikhar Shukla <shikharshukla678@gmail.com>
2026-06-08 17:52:33 +0530

    solve lint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} dark`}>
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-          rel="stylesheet"
-        />
-      </head>
       <body className="noise-bg min-h-screen antialiased">{children}</body>
     </html>
   );
